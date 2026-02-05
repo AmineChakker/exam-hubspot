f515a62f85ea2ff6735086dcc7ba6a732ba89a8e
Amine Chakker <aminechakker18@gmail.com>
2026-02-05 12:11:09 +0100

    submit the exam

--- index.js
+++ index.js
@@ -1,71 +1,78 @@
-const express = require('express');
-const axios = require('axios');
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// Example: pets
+const CUSTOM_OBJECT = "pets";
 
-// * Code for Route 1 goes here
+// Common headers
+const headers = {
+  Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+  "Content-Type": "application/json",
+};
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+/**
+ * ROUTE 1
+ * Homepage – display custom object records in a table
+ */
+app.get("/", async (req, res) => {
+  const endpoint = `https://api.hubspot.com/crm/v3/objects/${CUSTOM_OBJECT}?properties=name,description,age`;
 
-// * Code for Route 2 goes here
+  try {
+    const response = await axios.get(endpoint, { headers });
+    const records = response.data.results;
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+    res.render("homepage", {
+      title: "Custom Objects | Integrating With HubSpot I Practicum",
+      records,
+    });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.send("Error fetching custom object data");
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+/**
+ * ROUTE 2
+ * Render form to create a new custom object record
+ */
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+  });
 });
-*/
 
+/**
+ * ROUTE 3
+ * Handle form submission and create a new custom object record
+ */
+app.post("/update-cobj", async (req, res) => {
+  const newRecord = {
+    properties: {
+      name: req.body.name,
+      description: req.body.description,
+      age: req.body.age,
+    },
+  };
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+  const endpoint = `https://api.hubspot.com/crm/v3/objects/${CUSTOM_OBJECT}`;
+
+  try {
+    await axios.post(endpoint, newRecord, { headers });
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.send("Error creating custom object record");
+  }
+});
+
+// 🚀 Server
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
